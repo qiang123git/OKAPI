851e9bfaec19a148dc19b74621d1b87b502b0418
LGwolf <320119@163.com>
2020-09-18 22:19:24 +0800

    提交token为空时修改密码

--- cases/test_appkey.py
+++ cases/test_appkey.py
@@ -1,44 +1,40 @@
-
-import unittest
+import os
 import ddt
+import unittest
 import requests
-
-from lib.utils import *
 from setting import *
+from lib.utils import *
 
 @ddt.ddt
-class Alter(unittest.TestCase):
-    @ddt.file_data(os.path.join(DATA_PATH,'AlterPassword.yaml'))
-    def test_alter(self,**case):
+class AppKey(unittest.TestCase):
+    @ddt.file_data(os.path.join(DATA_PATH,'appkey为空修改密码.yaml'))
+    def test_appkey(self,**case):
         print(case)
         url=case.get('url')
-        print(url)
         method=case.get('method')
         data=case.get('data')
-        print(data)
         check=case.get('check')
-        print(check)
         if 'old_password' in data:
             data['old_password']=hash_code(data['old_password'])
             print(data['old_password'])
-        if 'new_password' in data:
+        if 'new_passworf' in data:
             data['new_password'] = hash_code(data['new_password'])
             print(data['new_password'])
-
         if method.lower() == 'post':
             res=requests.post(url,data=data)
             resp=res.text
             print(resp)
         else:
-            res=requests.get(url,params=data)
+            res=requests.get(url,data=data)
             resp=res.text
             print(resp)
-
         result=set_res_data(resp)
         print(result)
-        for i in check:
-            print(i)
-            self.assertIn(i,result)
+        i=check[0]
+        print(i)
+        self.assertIn(i,result)
 
 if __name__ == '__main__':
     unittest.main()
+
+
