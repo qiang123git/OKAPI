4a197395d1e773e4da0f17563226657033d937d0
libo <lbwyh_1@126.com>
2020-09-18 10:27:09 +0800

    "小白接口会员列表单个正向用例"

---
+++ setting.py
@@ -0,0 +1,10 @@
+import os
+#设置目录的绝对路径
+BASE_PATH=os.path.dirname(os.path.abspath(__file__))
+#yaml测试用例存放位置
+DATA_PATH=os.path.join(BASE_PATH,'data')
+
+#unittest测试类文件存放位置
+CASE_PATH=os.path.join(BASE_PATH,'cases')
+#测试报告路径
+REPORT_PATH=os.path.join(BASE_PATH,'report')
